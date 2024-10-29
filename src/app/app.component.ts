ee3def4d0f0bd858ca27b758316c8bb6b41d1a37
Caroline <wanguic49@gmail.com>
2024-10-29 11:57:21 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { DonateComponent } from './donate/donate.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet ,HomeComponent,HeaderComponent,DonateComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
